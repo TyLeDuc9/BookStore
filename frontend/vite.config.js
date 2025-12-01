247a33bac53d788257b5f94e70e2f62b38b0a388
TyLeDuc9 <ducty9963@gmail.com>
2025-12-02 00:36:15 +0700

    change file vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   define: {
-    global: 'window', 
+  define: {
+    global: 'window',
   },
-})
+  preview: {
+    host: true, // bind ra 0.0.0.0
+    port: 4173, // có thể để bất kỳ port nào Render cấp
+    allowedHosts: ['bookstore-7ryn.onrender.com'] // thêm domain Render
+  }
+});
