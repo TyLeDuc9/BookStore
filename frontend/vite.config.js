6451801419bbd4870f75db1b0b96b8073ce9d5a0
TyLeDuc9 <ducty9963@gmail.com>
2025-12-02 10:55:42 +0700

    updated viteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,4 +7,10 @@ export default defineConfig({
    define: {
     global: 'window', 
   },
+  preview: {
+    host: 'localhost',
+    port: 5000,
+
+    allowedHosts: ['bookstore-ixlc.onrender.com'], 
+  },
 })
