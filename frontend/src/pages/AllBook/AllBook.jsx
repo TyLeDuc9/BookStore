2a2c6e7a505a38972894f318ed743edae6b87ae6
TyLeDuc9 <ducty9963@gmail.com>
2025-12-03 13:10:42 +0700

    updated blogbook 2

--- frontend/src/pages/AllBook/AllBook.jsx
+++ frontend/src/pages/AllBook/AllBook.jsx
@@ -11,7 +11,6 @@ import { GenreList } from "../../components/Genre/GenreList";
 import { GenrePublisher } from "../../components/Genre/GenrePublisher";
 import { GenrePrice } from '../../components/Genre/GenrePrice'
 import { BannerPages } from "../../components/Banner/BannerPages";
-import { BlogBook } from "../../components/TopBook/BlogBook";
 export const AllBook = () => {
     const { setComponentsLoading } = useLoading();
     const [searchParams, setSearchParams] = useSearchParams();
@@ -76,9 +75,8 @@ export const AllBook = () => {
                         </div>
                     )}
                 </div>
-             
+
             </div>
-                  <BlogBook />
         </section>
     );
 };
