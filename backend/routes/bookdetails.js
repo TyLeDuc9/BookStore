c6df795b3bb699b4652093cc9a80a58370edbe82
TyLeDuc9 <ducty9963@gmail.com>
2026-02-28 07:24:08 +0700

    fix bookdetail route

--- backend/routes/bookdetails.js
+++ backend/routes/bookdetails.js
@@ -3,19 +3,44 @@ const router = express.Router();
 const uploadBookImage = require('../middlewares/uploadBook');
 const bookdetailsController = require('../controllers/bookdetailsController');
 const { verifyToken, verifyRole } = require('../middlewares/authMiddleware');
-router.post('/',verifyToken,verifyRole(['admin', 'employee']), uploadBookImage, bookdetailsController.createBookDetail);
-router.put('/:id',verifyToken,verifyRole(['admin', 'employee']), uploadBookImage, bookdetailsController.updateBookDetail);
-router.get("/search", bookdetailsController.searchBooks);
-router.post("/buynow", bookdetailsController.buyNowCheckout);
-router.get('/latest', bookdetailsController.getLatestBooks);    
-router.get('/:id', bookdetailsController.getBookDetailById);
-router.get('/related/:id/:slug', bookdetailsController.getRelatedBooks);
-router.get('/category/:id/:slug', bookdetailsController.getBooksByCategory);
-router.get('/:id/:slug', bookdetailsController.getBookDetail);
-router.delete("/:id",verifyToken, verifyRole(['admin']), bookdetailsController.deleteBookDetail);
 
 router.get('/random', bookdetailsController.getRandomBooks);
-router.get('/', bookdetailsController.getAllBookDetails);
+router.get('/latest', bookdetailsController.getLatestBooks);
+router.get('/search', bookdetailsController.searchBooks);
+router.post('/buynow', bookdetailsController.buyNowCheckout);
+router.get('/related/:id/:slug', bookdetailsController.getRelatedBooks);
+router.get('/category/:id/:slug', bookdetailsController.getBooksByCategory);
 router.get('/all/:id/:slug', bookdetailsController.getAllBooksByCategory);
-router.get("/publisher/:id/:slug", bookdetailsController.getAllBooksByPublisher);
+router.get('/publisher/:id/:slug', bookdetailsController.getAllBooksByPublisher);
+
+
+router.post(
+  '/',
+  verifyToken,
+  verifyRole(['admin', 'employee']),
+  uploadBookImage,
+  bookdetailsController.createBookDetail
+);
+
+router.put(
+  '/:id',
+  verifyToken,
+  verifyRole(['admin', 'employee']),
+  uploadBookImage,
+  bookdetailsController.updateBookDetail
+);
+
+
+router.get('/:id/:slug', bookdetailsController.getBookDetail);
+router.get('/:id', bookdetailsController.getBookDetailById);
+
+router.delete(
+  '/:id',
+  verifyToken,
+  verifyRole(['admin']),
+  bookdetailsController.deleteBookDetail
+);
+
+router.get('/', bookdetailsController.getAllBookDetails);
+
 module.exports = router;
