5d6a71bd509d448a2afc2735ad71a13105ef0632
TyLeDuc9 <ducty9963@gmail.com>
2026-03-05 23:03:37 +0700

    fix slider blog

--- frontend/src/components/TopBook/BlogBook.jsx
+++ frontend/src/components/TopBook/BlogBook.jsx
@@ -1,63 +1,10 @@
 import React, { useEffect } from 'react';
 import { useAllBlog } from "../../hooks/useAllBlog";
-import Slider from 'react-slick';
 import { FaCalendarAlt } from "react-icons/fa";
 import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
 import { useLoading } from '../../context/LoadingContext';
 import { ComponentLoading } from "../../components/Loading/ComponentLoading";
-// 👉 Custom arrows
-const NextArrow = ({ onClick }) => (
-  <div
-    className="absolute right-0 top-1/2 transform -translate-y-1/2 cursor-pointer z-10 
-    bg-gray-100/80 p-2 rounded-full hover:bg-gray-200 transition"
-    onClick={onClick}
-  >
-    <MdArrowForwardIos className="text-gray-600 lg:text-xl text-lg" />
-  </div>
-);
-
-const PrevArrow = ({ onClick }) => (
-  <div
-    className="absolute left-0 top-1/2 transform -translate-y-1/2 cursor-pointer z-10 
-    bg-gray-100/80 p-2 rounded-full hover:bg-gray-200 transition"
-    onClick={onClick}
-  >
-    <MdArrowBackIos className="text-gray-600 lg:text-xl text-lg" />
-  </div>
-);
-
-// 👉 Slider settings
-const settings = {
-  dots: false,
-  infinite: false,
-  speed: 500,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: false,
-  autoplaySpeed: 3000,
-  arrows: true,
-  nextArrow: <NextArrow />,
-  prevArrow: <PrevArrow />,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: true,
-      },
-    },
-    {
-      breakpoint: 767,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        arrows: false, // Ẩn arrow trên mobile (nếu muốn)
-      },
-    },
-  ],
-};
 
 
 
@@ -80,7 +27,7 @@ export const BlogBook = () => {
 
   return (
     <div className="relative w-[90%] mx-auto lg:my-12 sm:my-12 my-6">
-      <Slider {...settings}>
+      <div className='grid lg:grid-cols-4 grid-cols-2 '>
         {blogs.map((item) => (
           <div key={item._id} className="px-4">
             <div className="bg-gray-100 rounded-lg overflow-hidden flex flex-col lg:h-80 sm:h-80 h-48">
@@ -113,7 +60,8 @@ export const BlogBook = () => {
             </div>
           </div>
         ))}
-      </Slider>
+      </div>
+
     </div>
   );
 };
